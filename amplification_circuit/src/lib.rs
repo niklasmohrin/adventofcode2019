591d5267a84b52d315647ed1bbe667948866b310
Niklas Mohrin <niklas.mohrin@gmail.com>
2020-03-03 18:10:33 +0100

    moved threading stuff from amplification circuit crate into channel io crate and build wrapper around it

--- amplification_circuit/src/lib.rs
+++ amplification_circuit/src/lib.rs
@@ -1,10 +1,7 @@
 //! Implementation of the Amplifier Circuit described in day 7 of the 2019 [Advent of Code](adventofcode.com)
 
-use intcode_channel_io::{IntcodeChannelIo, Message};
-use intcode_computer::{read_program_from_file, run_program, Opcode};
-use std::cell::RefCell;
-use std::sync::mpsc;
-use std::thread;
+use intcode_channel_io::IntcodeThread;
+use intcode_computer::{read_program_from_file, Opcode};
 
 /// An amplifier, that is initially set to a phase mode
 /// and then able to receive a signal and amplify (send) it.
@@ -12,32 +9,19 @@ use std::thread;
 /// which it gets through an instance of IntcodeChannelIo.
 pub struct Amplifier {
     phase_setting: u8,
-    pub handle: thread::JoinHandle<()>,
-    sender: mpsc::Sender<Message>,
-    receiver: mpsc::Receiver<Message>,
-    exited: RefCell<bool>,
+    thread: IntcodeThread,
 }
 
 impl Amplifier {
     pub fn new(program_file: &str, phase_setting: u8) -> Amplifier {
-        let mut program = read_program_from_file(&program_file);
+        let program = read_program_from_file(&program_file);
+        let identifier = Some(format!("Amp {}", phase_setting));
 
-        // set up bidirectional channel
-        let (host_sender, thread_receiver) = mpsc::channel();
-        let (thread_sender, host_receiver) = mpsc::channel();
-        let inout = IntcodeChannelIo::new(thread_sender, thread_receiver);
-
-        let handle = thread::spawn(move || {
-            run_program(&mut program, &inout);
-            inout.send_exit_signal();
-        });
+        let thread = IntcodeThread::new(program, identifier);
 
         let amp = Amplifier {
             phase_setting,
-            handle,
-            sender: host_sender,
-            receiver: host_receiver,
-            exited: RefCell::new(false),
+            thread,
         };
 
         // initialize intcode program with the phase setting (further input will be the signals)
@@ -48,38 +32,15 @@ impl Amplifier {
 
     /// Sends an Opcode to the underlying worker thread.
     pub fn send(&self, value: Opcode) {
-        println!(
-            "[Amp {}]: sending {} to worker...",
-            self.phase_setting, value
-        );
-        self.sender
-            .send(Message::Data(value))
-            .unwrap_or_else(|err| {
-                println!(
-                    "[Amp {}]: appareanlty down; couldn't send; err: {}",
-                    self.phase_setting, err
-                );
-            });
+        self.thread.send(value);
     }
 
     /// Receives an Opcode from the worker and maybe update internal exited field.
     pub fn recv(&self) -> Option<Opcode> {
-        print!("[Amp {}]: ", self.phase_setting);
-        match self.receiver.recv().unwrap() {
-            Message::Data(val) => {
-                println!("received {} from worker...", val);
-                Some(val)
-            }
-            Message::Exited => {
-                println!("exiting");
-                *self.exited.borrow_mut() = true;
-                None
-            }
-        }
+        self.thread.recv()
     }
 
-    /// Public getter for (internally mutable) "exited" field.
     pub fn has_exited(&self) -> bool {
-        *self.exited.borrow()
+        self.thread.has_exited()
     }
 }
--- intcode_channel_io/src/lib.rs
+++ intcode_channel_io/src/lib.rs
@@ -1,7 +1,9 @@
 //! IO over mpsc channels for the [Advent of Code](adventofcode.com) Intcode Computer.
 
-use intcode_computer::{IntcodeIo, Opcode};
+use intcode_computer::{run_program, IntcodeIo, Opcode, ProgramMemory};
+use std::cell::RefCell;
 use std::sync::mpsc;
+use std::thread;
 
 /// Message type to be sent between threads.
 pub enum Message {
@@ -38,5 +40,73 @@ impl IntcodeIo for IntcodeChannelIo {
 
     fn write(&self, value: &Opcode) {
         self.sender.send(Message::Data(value.clone())).unwrap();
+    }
+}
+
+/// The interface for the worker thread
+pub struct IntcodeThread {
+    pub handle: thread::JoinHandle<ProgramMemory>,
+    sender: mpsc::Sender<Message>,
+    receiver: mpsc::Receiver<Message>,
+    exited: RefCell<bool>,
+    pub identifier: String,
+}
+
+impl IntcodeThread {
+    pub fn new(mut program: ProgramMemory, identifier: Option<String>) -> IntcodeThread {
+        // set up bidirectional channel
+        let (host_sender, thread_receiver) = mpsc::channel();
+        let (thread_sender, host_receiver) = mpsc::channel();
+        let inout = IntcodeChannelIo::new(thread_sender, thread_receiver);
+
+        let handle = thread::spawn(move || {
+            run_program(&mut program, &inout);
+            inout.send_exit_signal();
+            program
+        });
+
+        let identifier = identifier.unwrap_or(String::from("Thread ?"));
+
+        IntcodeThread {
+            handle,
+            sender: host_sender,
+            receiver: host_receiver,
+            exited: RefCell::new(false),
+            identifier,
+        }
+    }
+
+    /// Sends an Opcode to the underlying worker thread.
+    pub fn send(&self, value: Opcode) {
+        println!("[{}]: sending <{}> to worker", self.identifier, value);
+        self.sender
+            .send(Message::Data(value))
+            .unwrap_or_else(|err| {
+                println!(
+                    "{}: worker appareanlty down; couldn't send; err: '{}'",
+                    self.identifier, err
+                );
+            });
+    }
+
+    /// Receives an Opcode from the worker and maybe update internal exited field.
+    pub fn recv(&self) -> Option<Opcode> {
+        print!("[{}]: ", self.identifier);
+        match self.receiver.recv().unwrap() {
+            Message::Data(val) => {
+                println!("received <{}> from worker...", val);
+                Some(val)
+            }
+            Message::Exited => {
+                println!("worker has exited");
+                *self.exited.borrow_mut() = true;
+                None
+            }
+        }
+    }
+
+    /// Public getter for (internally mutable) "exited" field.
+    pub fn has_exited(&self) -> bool {
+        *self.exited.borrow()
     }
 }
