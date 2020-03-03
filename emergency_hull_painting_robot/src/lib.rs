489aa039833c6a19e6421130ac9f9f5e3547527c
Niklas Mohrin <niklas.mohrin@gmail.com>
2020-03-03 20:23:42 +0100

    added documentation to Emergency Hull Painting Robot

--- emergency_hull_painting_robot/src/lib.rs
+++ emergency_hull_painting_robot/src/lib.rs
@@ -1,7 +1,10 @@
+//! Rust implementation of the Emergency Hull Painting Robot described on day 11 of [Advent of Code 2019](adventofcode.com).
+
 use intcode_channel_io::IntcodeThread;
 use intcode_computer::{Opcode, ProgramMemory};
 use std::collections::HashMap;
 
+/// Orientation of the robot.
 pub enum Orientation {
     Up,
     Down,
@@ -9,6 +12,7 @@ pub enum Orientation {
     Right,
 }
 
+/// Direction, the robot can turn in after a move.
 pub enum TurningDirection {
     Left,
     Right,
@@ -34,8 +38,10 @@ impl Orientation {
     }
 }
 
+/// Range of a single axis of the drawing board.
 type Axis = i32;
 
+/// A coordinate on the drawing board.
 #[derive(PartialEq, Eq, Hash, Clone)]
 pub struct Coordinate(Axis, Axis);
 
@@ -57,11 +63,14 @@ impl Coordinate {
     }
 }
 
+/// Color, that the robot can paint.
 #[derive(Clone, Copy)]
 pub enum Color {
     Black,
     White,
 }
+
+// Conversion between Opcode and Color, both ways
 
 impl From<Color> for Opcode {
     fn from(color: Color) -> Self {
@@ -82,6 +91,12 @@ impl From<Opcode> for Color {
     }
 }
 
+/// The Emergency Hull Painting Robot as described on day 11 of [Advent of Code](adventofcode.com).
+/// This robot will spin up a thread for its brain, running a provided Intcode Computer program.
+/// Input and output of that computer instance is handled via the interface of the IntcodeThread,
+/// which uses std::mpsc channels.
+/// The robot records the drawn canvas as well as the total number of moves.
+/// Internally, it keeps track of its position and orientation.
 pub struct EmergencyHullPaintingRobot {
     pub painted_panels: HashMap<Coordinate, Color>,
     thread: IntcodeThread,
@@ -94,6 +109,10 @@ const WHITE_PAINTING_CHAR: char = '#';
 const BLACK_PAINTING_CHAR: char = ' ';
 
 impl EmergencyHullPaintingRobot {
+    /// Creates a new EmergencyHullPaintingRobot.
+    /// The instance will take ownership of the ProgramMemory,
+    /// since it will be passed onto the IntcodeThread, that transfers ownership
+    /// into another thread.
     pub fn new(program: ProgramMemory) -> EmergencyHullPaintingRobot {
         let painted_panels = HashMap::new();
         let identifier = Some(String::from("Robot"));
@@ -111,34 +130,42 @@ impl EmergencyHullPaintingRobot {
         }
     }
 
+    /// Starts communication with the underlying robot brain.
+    /// The color of the starting position must be provided.
     pub fn run(&mut self, starting_panel: Color) {
         self.painted_panels
             .insert(self.position.clone(), starting_panel);
 
         while !self.thread.has_exited() {
+            // First, send the color underneath the robot to the brain.
             let current_color = *self
                 .painted_panels
                 .get(&self.position)
                 .unwrap_or(&Color::Black);
             self.thread.send(current_color.into());
 
+            // Then, the brain will send back the painted color, which is saved.
             let color_painted: Color = match self.thread.recv() {
                 Some(opcode) => opcode.into(),
+                // The thread has been shut down.
                 None => break,
             };
-
             self.painted_panels
                 .insert(self.position.clone(), color_painted);
 
+            // After that, the brain signals in which direction it turned,
+            // it will always turn 90 degress to one side.
             let turning_direction = match self.thread.recv() {
                 Some(direction) => match direction {
                     0 => TurningDirection::Left,
                     1 => TurningDirection::Right,
                     _ => panic!("[Robot]: got invalid direction from worker"),
                 },
+                // The thread has been shut down.
                 None => break,
             };
 
+            // Finally, update the internal state.
             self.orientation.turn(&turning_direction);
             self.position.walk(&self.orientation);
 
@@ -146,6 +173,7 @@ impl EmergencyHullPaintingRobot {
         }
     }
 
+    /// Prints the painted canvas to the console.
     pub fn print_painting(&self) {
         let coordinates: Vec<&Coordinate> = self.painted_panels.keys().collect();
         let xs: Vec<Axis> = coordinates.iter().map(|coord| coord.0).collect();
